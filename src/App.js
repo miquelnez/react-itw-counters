e633f9b98654ba7a6296525c2d4d73768ca1d4c5
Miquel Martinez <miquel.martinez@amaris.com>
2021-12-27 10:35:04 +0100

    add launch.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const checkVar = "perro";
+  console.log(checkVar);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>

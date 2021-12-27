9c764e63571f9815682f8aab87c8752f1e6b1bd7
Miquel Martinez <miquel.martinez@amaris.com>
2021-12-27 11:15:41 +0100

    add counter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import './App.css';
+// const { useState } = React;
+
+function Counter() {
+  return (
+    <div className="counter">
+      <span>show counter value here</span>
+      <div className="counter-controls">
+        <button type="button" className="button is-danger is-small">-</button>
+        <button type="button" className="button is-success is-small">+</button>
+      </div>
+    </div>
+  );
+}
 
 function App() {
+  const data = [
+    { id: 1, value: 0 },
+    { id: 2, value: 0 },
+    { id: 3, value: 0 },
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
@@ -13,6 +32,14 @@ function App() {
           ITW test
         </a>
       </header>
+      <main>
+        <div>
+          <h2>Counters</h2>
+          {data.map((counter) => (
+            <Counter key={counter.id} id={counter.id} value={counter.value} />
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
